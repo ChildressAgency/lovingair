92c57ed4acf58ac2399f23d45e3f4f1c1dffcfe9
halfg33k <johnsonam17@gmail.com>
2018-09-13 11:41:10 -0400

    built header

--- header.php
+++ header.php
@@ -28,5 +28,40 @@
 </head>
 <body>
     <header>
-        
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg">
+                <a href="<?php echo esc_url( home_url() ); ?>" class="navbar-brand"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png"></a>
+                <div class="header__main">
+                    <div class="header__action">
+                        <div class="header__quote">
+                            <p class="header__heading">Same Day Service Available</p>
+                            <a href="#" class="btn btn-primary">Request A Quote</a>
+                        </div>
+                        <div class="header__call">
+                            <p class="header__heading">Call Us!</p>
+                            <img class="img-fluid" src="<?php echo get_stylesheet_directory_uri(); ?>/images/phone.png">
+                            <p class="header__phone">(540) 399-1300</p>
+                        </div>
+                    </div>
+                    <div class="header__menu">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <i class="fas fa-bars"></i>
+                        </button>
+                        <div class="collapse navbar-collapse">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item active"><a href="#" class="nav-link">HOME</a></li>
+                                <li class="nav-item"><a href="#" class="nav-link">ABOUT US</a></li>
+                                <li class="nav-item"><a href="#" class="nav-link">HVAC SERVICES</a></li>
+                                <li class="nav-item"><a href="#" class="nav-link">COMMERCIAL SERVICES</a></li>
+                                <li class="nav-item"><a href="#" class="nav-link">CONTACT US</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        <div class="header__announcement">
+            <p>September Special - Save 15% on all Services!</p>
+        </div>
     </header>
