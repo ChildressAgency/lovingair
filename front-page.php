3961a97ec21b030890755bfe59c5ea41466ed299
halfg33k <johnsonam17@gmail.com>
2018-09-14 10:42:57 -0400

    fixed carousel styling; added images to carousel

--- front-page.php
+++ front-page.php
@@ -19,23 +19,24 @@
             <div class="carousel-inner">
                 <div class="carousel-item active" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/carousel-1.png');">
                     <div class="carousel-caption">
-                        <h2>HVAC Specialists</h2>
-                        <p>Serving the Greater region of Fredericksburg</p>
-                        <a href="#" class="btn btn-secondary">See what we have to offer</a>
+                        <h2 class="carousel__heading">HVAC Specialists</h2>
+                        <p class="carousel__subheading">Serving the Greater region of Fredericksburg</p>
+                        <a href="#" class="btn btn-secondary carousel__btn">See what we have to offer</a>
                     </div>
                 </div>
-                <div class="carousel-item" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/carousel-1.png');">
+                <div class="carousel-item" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/carousel-2.png');">
                     <div class="carousel-caption">
-                        <h2>HVAC Specialists</h2>
-                        <p>Serving the Greater region of Fredericksburg</p>
-                        <a href="#" class="btn btn-secondary">See what we have to offer</a>
+                        <h2 class="carousel__heading">Experienced Technicians</h2>
+                        <p class="carousel__subheading">You Won't Like Us, You'll Love Us!</p>
+                        <p>Our experienced technicians solve complex home and business comfort problems quickly and affordably while respecting your time and property. When you call the Loving Air Inc. team, you’ll speak directly to a member of our team rather than being funneled through a maze of voice recordings.</p>
+                        <a href="#" class="btn btn-secondary carousel__btn">See what we have to offer</a>
                     </div>
                 </div>
-                <div class="carousel-item" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/carousel-1.png');">
+                <div class="carousel-item" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/carousel-3.png');">
                     <div class="carousel-caption">
-                        <h2>HVAC Specialists</h2>
-                        <p>Serving the Greater region of Fredericksburg</p>
-                        <a href="#" class="btn btn-secondary">See what we have to offer</a>
+                        <h2 class="carousel__heading">Available 24/7</h2>
+                        <p class="carousel__subheading">We offer affordable emergency residential and commercial HVAC service if needed.</p>
+                        <a href="#" class="btn btn-secondary carousel__btn">See what we have to offer</a>
                     </div>
                 </div>
             </div>
