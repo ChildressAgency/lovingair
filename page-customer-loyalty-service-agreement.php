e64bd3df5ebe60a5c39233aca31854ddb0942c10
halfg33k <johnsonam17@gmail.com>
2018-09-19 17:28:42 -0400

    added commercial services page (non-responsive)

--- page-customer-loyalty-service-agreement.php
+++ page-customer-loyalty-service-agreement.php
@@ -6,90 +6,92 @@
         </div>
     </div>
 
-    <div class="container">
-        <div class="section">
-            <div class="section__heading">
-                <h3>HVAC</h3>
-                <h2>Service & Maintenance</h2>
+    <article>
+        <div class="container">
+            <div class="section">
+                <div class="section__heading">
+                    <h3>HVAC</h3>
+                    <h2>Service & Maintenance</h2>
+                </div>
+                <h4>Serving the region of Fredericksburg and surrounding areas</h4>
+        
+                <p>At Loving Air Inc, we offer unique service and maintenance agreements for the convenience of our customers. Maintaining and servicing your HVAC systems is crucial to keep them in proper working order. Let us help keep your HVAC system in shape.</p>
+        
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/loyalty-1.png" alt="" class="img-center">
             </div>
-            <h4>Serving the region of Fredericksburg and surrounding areas</h4>
-
-            <p>At Loving Air Inc, we offer unique service and maintenance agreements for the convenience of our customers. Maintaining and servicing your HVAC systems is crucial to keep them in proper working order. Let us help keep your HVAC system in shape.</p>
-
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/loyalty-1.png" alt="" class="img-center">
+        
+            <div class="section section--thin">
+                <div class="section__header">
+                    <h2>Maintenance Agreement</h2>
+                </div>
+        
+                <p>Your heating and cooling system is just like anything else you own. Proper maintenance will help your system perform at its peak performance.</p>
+                
+                <h5>Proper Mainenance helps with:</h5>
+        
+                <div class="row">
+                    <div class="col-4">
+                        <ul>
+                            <li>Lower utility bills</li>
+                            <li>Improve indoor comfort</li>
+                            <li>Extended equipment life</li>
+                        </ul>
+                    </div>
+                    <div class="col-4">
+                        <ul>
+                            <li>Reduce major failures</li>
+                            <li>Priority scheduling</li>
+                            <li>15% discount off service calls</li>
+                        </ul>
+                    </div>
+                    <div class="col-4">
+                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/loyalty-icon-black.png" alt="" class="img-center">
+                    </div>
+                </div>
+            </div>
+        
+            <div class="slash-separator"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slash-separator.png"></div>
+        
+            <div class="section">
+                <div class="section__heading">
+                    <h2>Service Agreement</h2>
+                </div>
+        
+                <p>With our Service Agreement, we will come out every 6 months and fully inspect your system to ensure your equipment is running efficiently.</p>
+                <p>Before entering into a Service Agreement, if the customer invests in making the system fully operational, per the manufacturer's recommendation, we will provide the customer with 15% off of those repairs needed at that time.</p>
+        
+                <h5>We perform maintenance on the following items:</h5>
+        
+                <div class="row">
+                    <div class="col-6">
+                        <ul>
+                            <li>Clean and inspect burner assembly</li>
+                            <li>Check and adjust pressures if needed</li>
+                            <li>Clean ignition assembly</li>
+                            <li>Inspect heat exchanger, flue, and safeties</li>
+                            <li>Measure temperature rise and/or temperature difference</li>
+                            <li>Check refrigerant levels</li>
+                        </ul>
+                    </div>
+                    <div class="col-6">
+                        <ul>
+                            <li>Clean condenser and evaporator coils</li>
+                            <li>Check all electrical connections and components</li>
+                            <li>Clena out condensate drain lines</li>
+                            <li>Check all amp draws on all electrical motors and compressors</li>
+                            <li>Check filter condition</li>
+                            <li>Confirm that system is fully functional</li>
+                        </ul>
+                    </div>
+                </div>
+        
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/loyalty-2.png" alt="" class="img-center mb-5">
+        
+                <p class="text-center">Participating business in Rappahannock Electric Cooperative Co-op Connection Program and BBB Accredited Business</p>
+                <img class="img-center" src="<?php echo get_stylesheet_directory_uri(); ?>/images/rappahannock-electric-cooperative-logo.png">
+                <img class="img-center" src="<?php echo get_stylesheet_directory_uri(); ?>/images/bbb.png">
+            </div>
         </div>
-
-        <div class="section section--thin">
-            <div class="section__header">
-                <h2>Maintenance Agreement</h2>
-            </div>
-
-            <p>Your heating and cooling system is just like anything else you own. Proper maintenance will help your system perform at its peak performance.</p>
-            
-            <h5>Proper Mainenance helps with:</h5>
-
-            <div class="row">
-                <div class="col-4">
-                    <ul>
-                        <li>Lower utility bills</li>
-                        <li>Improve indoor comfort</li>
-                        <li>Extended equipment life</li>
-                    </ul>
-                </div>
-                <div class="col-4">
-                    <ul>
-                        <li>Reduce major failures</li>
-                        <li>Priority scheduling</li>
-                        <li>15% discount off service calls</li>
-                    </ul>
-                </div>
-                <div class="col-4">
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/loyalty-icon-black.png" alt="" class="img-center">
-                </div>
-            </div>
-        </div>
-
-        <div class="slash-separator"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slash-separator.png"></div>
-
-        <div class="section">
-            <div class="section__heading">
-                <h2>Service Agreement</h2>
-            </div>
-
-            <p>With our Service Agreement, we will come out every 6 months and fully inspect your system to ensure your equipment is running efficiently.</p>
-            <p>Before entering into a Service Agreement, if the customer invests in making the system fully operational, per the manufacturer's recommendation, we will provide the customer with 15% off of those repairs needed at that time.</p>
-
-            <h5>We perform maintenance on the following items:</h5>
-
-            <div class="row">
-                <div class="col-6">
-                    <ul>
-                        <li>Clean and inspect burner assembly</li>
-                        <li>Check and adjust pressures if needed</li>
-                        <li>Clean ignition assembly</li>
-                        <li>Inspect heat exchanger, flue, and safeties</li>
-                        <li>Measure temperature rise and/or temperature difference</li>
-                        <li>Check refrigerant levels</li>
-                    </ul>
-                </div>
-                <div class="col-6">
-                    <ul>
-                        <li>Clean condenser and evaporator coils</li>
-                        <li>Check all electrical connections and components</li>
-                        <li>Clena out condensate drain lines</li>
-                        <li>Check all amp draws on all electrical motors and compressors</li>
-                        <li>Check filter condition</li>
-                        <li>Confirm that system is fully functional</li>
-                    </ul>
-                </div>
-            </div>
-
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/loyalty-2.png" alt="" class="img-center mb-5">
-
-            <p class="text-center">Participating business in Rappahannock Electric Cooperative Co-op Connection Program and BBB Accredited Business</p>
-            <img class="img-center" src="<?php echo get_stylesheet_directory_uri(); ?>/images/rappahannock-electric-cooperative-logo.png">
-            <img class="img-center" src="<?php echo get_stylesheet_directory_uri(); ?>/images/bbb.png">
-        </div>
-    </div>
+    </article>
 
 <?php get_footer(); ?>
