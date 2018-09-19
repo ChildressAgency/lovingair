337e2dac12242fd95a4ba45ab3624661f65d66c7
halfg33k <johnsonam17@gmail.com>
2018-09-19 14:03:44 -0400

    added Air Conditioning Services Page

---
+++ page-air-conditioning-services.php
@@ -0,0 +1,66 @@
+<?php get_header(); ?>
+
+    <div class="page-header" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/ac-services-hero.png');">
+        <div class="dark-overlay">
+            <h1><?php the_title(); ?></h1>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="section">
+            <div class="section__heading">
+                <h3>Residential</h3>
+                <h2>Air Conditioning</h2>
+            </div>
+            <h4>Serving the region of Fredericksburg and surrounding areas</h4>
+            <h5>Air Conditioning Repairs, Service, and New Installation</h5>
+            <p>In the heart of summer, AC repairs are just about the last thing you want to think about. But sometimes it's unavoidable. The team at Loving Air Inc is committed to performing expert quality AC repair, maintenance, and installation throughout Fredericksburg, Spotsylvania, Stafford, King George, Culpeper, Orange, Locust Grove, Lake Anna, and surrounding areas.</p>
+        
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ac-services-1.png" class="img-center">
+        </div>
+
+        <div class="section section--thin">
+            <h3>Our Air Conditioning Team Can Help With</h3>
+            
+            <div class="row">
+                <div class="col-4">
+                    <ul>
+                        <li>Installation of new systems</li>
+                        <li>Air Conditioning Repairs</li>
+                        <li>Air Conditioning Maintenance</li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <ul>
+                        <li>Air Filtration Services</li>
+                        <li>UV Light Installation</li>
+                        <li>Humidity Control Systems</li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ac-services-icon-black.png" alt="" class="img-center">
+                </div>
+            </div>
+        </div>
+
+        <div class="slash-separator"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/slash-separator.png"></div>
+
+        <div class="section section--thin">
+            <div class="section__heading">
+                <h2>Satisfaction Guaranteed</h2>
+                <h4>Fully Licensed and Insured HVAC Specialists</h4>
+            </div>
+
+            <p>At any time of day, our experts will have you "chilling out" in the cool air of your repaired AC in no time. We are so certain of our workmanship that we guarantee it in writing for one full year. Our 100% Satisfaction Guarantee firmly documents our commitment to upholding the highest level of air conditioning service in the HVAC industry. Our dedication to providing outstanding service and putting our customer's needs first is why consumers trust us for their home comfort needs.</p>
+            <p>Check out our <a href="<?php echo esc_url( home_url( 'customer-loyalty-service-agreements' ) ); ?>">Customer Loyalty Service Agreements</a> to find out how it can help save you money on Air Conditioning.</p>
+        </div>
+
+        <div class="section">
+            <h3>AC Repair Services 24/7</h3>
+            <p>It seems like typically, air conditioners decide to stop operating when it's an inconvenience for you and that typically ends up on one of the hottest days of the year – of course. Let's not forget that it ends up happening when you're expecting visitors for the weekend or already have folks staying with you. That's just how it goes, at Loving Air Inc, we understand you need AC repair service right away, simply call us 24/7.</p>
+
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/ac-services-2.png" alt="" class="img-center">
+        </div>
+    </div>
+
+<?php get_footer(); ?>
