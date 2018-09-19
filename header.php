e4b63962e40f63255c95eaf9e69626b3c1f8b37e
halfg33k <johnsonam17@gmail.com>
2018-09-19 13:32:31 -0400

    hvac services as dropdown

--- header.php
+++ header.php
@@ -51,7 +51,14 @@
                             <ul class="navbar-nav ml-auto">
                                 <li class="nav-item <?php if( is_front_page() ){ echo 'active'; } ?>"><a href="<?php echo esc_url( home_url() ); ?>" class="nav-link">HOME</a></li>
                                 <li class="nav-item <?php if( is_page( 'about-us' ) ){ echo 'active'; } ?>"><a href="<?php echo esc_url( home_url( 'about-us' ) ); ?>" class="nav-link">ABOUT US</a></li>
-                                <li class="nav-item <?php if( is_page( 'hvac-services' ) ){ echo 'active'; } ?>"><a href="<?php echo esc_url( home_url( 'hvac-services' ) ); ?>" class="nav-link">HVAC SERVICES</a></li>
+                                <li class="nav-item dropdown <?php if( is_page( 'hvac-services' ) ){ echo 'active'; } ?>">
+                                    <a href="<?php echo esc_url( home_url( 'hvac-services' ) ); ?>" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">HVAC SERVICES</a>
+                                    <div class="dropdown-menu">
+                                        <a href="<?php echo esc_url( home_url( 'heating-services' ) ); ?>" class="dropdown-item">Heating Services</a>
+                                        <a href="<?php echo esc_url( home_url( 'air-conditioning-services' ) ); ?>" class="dropdown-item">Air Conditioning Services</a>
+                                        <a href="<?php echo esc_url( home_url( 'customer-service-loyalty-agreement' ) ); ?>" class="dropdown-item">Customer Service Loyalty Agreement</a>
+                                    </div>
+                                </li>
                                 <li class="nav-item <?php if( is_page( 'commercial-services' ) ){ echo 'active'; } ?>"><a href="<?php echo esc_url( home_url( 'commercial-services' ) ); ?>" class="nav-link">COMMERCIAL SERVICES</a></li>
                                 <li class="nav-item <?php if( is_page( 'contact-us' ) ){ echo 'active'; } ?>"><a href="<?php echo esc_url( home_url( 'contact-us' ) ); ?>" class="nav-link">CONTACT US</a></li>
                             </ul>
