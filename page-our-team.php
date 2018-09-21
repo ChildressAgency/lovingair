ba7f8100e1c31f80124a726085b8d882a5fa3143
halfg33k <johnsonam17@gmail.com>
2018-09-21 11:07:23 -0400

    added Our Team page

---
+++ page-our-team.php
@@ -0,0 +1,70 @@
+<?php get_header(); ?>
+
+    <div class="page-header" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/contact-hero.png');">
+        <div class="dark-overlay">
+            <h1><?php the_title(); ?></h1>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="section">
+            <div class="section__heading">
+                <h3>Licensed and Insured</h3>
+                <h2>HVAC Technicians</h2>
+            </div>
+
+            <h5>Serving the region of Fredericksburg and surrounding areas</h5>
+
+            <p>Loving Air Inc has been serving the Fredericksburg, Spotsylvania, Stafford, King George, Culpeper, Orange, Locust Grove, Lake Anna, and surrounding areas since 2014. We provide full-service heating and cooling services and pride ourselves on our quality work. Our owner, Shane Loving is a Master Technician, with over 20 years of experience in the industry. Shane, along with his team are dedicated to helping others and believe you should always be 100% satisfied all the time.</p>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/our-team-1.png" class="img-center">
+        </div>
+
+        <div class="section section--thin">
+            <div class="section__heading">
+                <h2>Our Team of Expert Technicians</h2>
+            </div>
+
+            <div class="row team__row">
+                <div class="col-6">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/richard-lohr.png" class="img-center">
+                    <p class="team__name">Richard Lohr</p>
+                </div>
+                <div class="col-6">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/trever-dill.png" class="img-center">
+                    <p class="team__name">Trever Dill</p>
+                </div>
+            </div>
+            <div class="row team__row">
+                <div class="col-6">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cody-brookman.png" class="img-center">
+                    <p class="team__name">Cody Brookman</p>
+                </div>
+                <div class="col-6">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/aaron-wright.png" class="img-center">
+                    <p class="team__name">Aaron Wright</p>
+                </div>
+            </div>
+            <div class="row team__row">
+                <div class="col-6">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/james-morris.png" class="img-center">
+                    <p class="team__name">James Morris</p>
+                </div>
+                <div class="col-6">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/deanna-skinner.png" class="img-center">
+                    <p class="team__name">Deanna Skinner</p>
+                </div>
+            </div>
+            <div class="row team__row">
+                <div class="col-6">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/no-name-1.png" class="img-center">
+                    <p class="team__name">Lorem Ipsum</p>
+                </div>
+                <div class="col-6">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/no-name-2.png" class="img-center">
+                    <p class="team__name">Lorem Ipsum</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php get_footer(); ?>
