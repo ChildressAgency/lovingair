cdfa73730cb4233bd63d65faad32bfb98879ffd9
halfg33k <johnsonam17@gmail.com>
2018-09-24 14:20:39 -0400

    responsive header

--- header.php
+++ header.php
@@ -44,40 +44,16 @@
                         </div>
                     </div>
                     <div class="header__menu">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#header-menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <i class="fas fa-bars"></i>
                         </button>
-                        <div class="collapse navbar-collapse">
+                        <div class="collapse navbar-collapse" id="header-menu">
                             <?php 
                             wp_nav_menu( array(
                                 'theme_location'    =>  'header_menu',
                                 'menu_class'        =>  'navbar-nav',
                                 'walker'            =>  new Custom_Nav_Walker()
                             ) ); ?>
-
-
-                            <!-- <ul class="navbar-nav ml-auto">
-                                <li class="nav-item <?php if( is_front_page() ){ echo 'active'; } ?>"><a href="<?php echo esc_url( home_url() ); ?>" class="nav-link">HOME</a></li>
-                                <li class="nav-item dropdown <?php if( is_page( 'about-us' ) ){ echo 'active'; } ?>">
-                                    <a href="#" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">ABOUT US</a>
-                                    <div class="dropdown-menu">
-                                        <a href="<?php echo esc_url( home_url( 'about-us' ) ); ?>" class="dropdown-item">About Us</a>
-                                        <a href="<?php echo esc_url( home_url( 'our-team' ) ); ?>" class="dropdown-item">Our Team</a>
-                                        <a href="<?php echo esc_url( home_url( 'why-choose-loving-air-inc' ) ); ?>" class="dropdown-item">Why Choose Loving Air Inc</a>
-                                        <a href="<?php echo esc_url( home_url( 'community-events' ) ); ?>" class="dropdown-item">Community Events</a>
-                                    </div>
-                                </li>
-                                <li class="nav-item dropdown <?php if( is_page( 'hvac-services' ) ){ echo 'active'; } ?>">
-                                    <a href="<?php echo esc_url( home_url( 'hvac-services' ) ); ?>" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">HVAC SERVICES</a>
-                                    <div class="dropdown-menu">
-                                        <a href="<?php echo esc_url( home_url( 'heating-services' ) ); ?>" class="dropdown-item">Heating Services</a>
-                                        <a href="<?php echo esc_url( home_url( 'air-conditioning-services' ) ); ?>" class="dropdown-item">Air Conditioning Services</a>
-                                        <a href="<?php echo esc_url( home_url( 'customer-loyalty-service-agreement' ) ); ?>" class="dropdown-item">Customer Loyalty Service Agreement</a>
-                                    </div>
-                                </li>
-                                <li class="nav-item <?php if( is_page( 'commercial-services' ) ){ echo 'active'; } ?>"><a href="<?php echo esc_url( home_url( 'commercial-services' ) ); ?>" class="nav-link">COMMERCIAL SERVICES</a></li>
-                                <li class="nav-item <?php if( is_page( 'contact-us' ) ){ echo 'active'; } ?>"><a href="<?php echo esc_url( home_url( 'contact-us' ) ); ?>" class="nav-link">CONTACT US</a></li>
-                            </ul> -->
                         </div>
                     </div>
                 </div>
